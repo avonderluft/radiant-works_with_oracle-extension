83d54d4f7a0ccc4e4f3f968e0d9c2a7a314ae8fc
Andrew vonderLuft <avonderluft@avlux.net>
2009-07-31 12:30:07 -0700

    clear default_scope on models that have it; add named_scope

--- lib/works_with_oracle/model_extensions.rb
+++ lib/works_with_oracle/model_extensions.rb
@@ -2,7 +2,7 @@ module WorksWithOracle::ModelExtensions
 
   def self.included(base)
     base.class_eval do
-      default_scope()
+      default_scoping.clear
       named_scope :default, :order => "name"     
     end
   end
